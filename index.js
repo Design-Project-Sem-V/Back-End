0e96cf3970f1c20bcc9512bd2a802835b31579b9
Umang Rathod <75923134+umang00775@users.noreply.github.com>
2022-10-30 19:31:28 +0530

    Login + Signup completed

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const User = require('./models/userModel')
 
 
 // Extra Settings :)
-// app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 mongoose.connect("mongodb+srv://Umang:umangrathod@cluster0.dfyu3vv.mongodb.net/HMS-DB", {useNewUrlParser :true});
@@ -23,36 +22,89 @@ app.get('/',(req,res)=>{
     res.send(`Successfully connected to port ${port}`);
 });
 
-
-app.post('/login', (req, res)=>{
-    const inptmail = req.body.usermail;
-    User.findOne({email:req.body.usermail}, (err, result)=>{
-        if(!err){
-            res.send(result.name);
-        }
-        else{
-            res.send("Something Went Wrong");
-        }
-    }); 
-});
-
-app.post('/test', (req,res)=>{
-    const nameX = String(req.body.name);
-    console.log(nameX)
-    res.send(nameX);
-})
-
 app.post('/signup_user', (req, res)=>{
     const NewUser = new User({
         name: req.body.name,
         phone: req.body.phone,
+        password:req.body.password,
         email: req.body.email,
         birthday: req.body.birthday,
-        gender: req.body.gender,
+        gender: _.capitalize(req.body.gender),
         bloodgroup: req.body.bloodgroup
     });
     NewUser.save();
     res.send("User details saved successfully");
+});
+
+
+app.post('/signup_doctor', (req, res)=>{
+    const NewDoctor = new Doctor({
+        name: req.body.name,
+        phone: req.body.phone,
+        password:req.body.password,
+        email: req.body.email,
+        birthday: req.body.birthday,
+        gender: _.capitalize(req.body.gender),
+        bloodgroup: req.body.bloodgroup,
+        experience: req.body.experience,
+        speciality: req.body.speciality,
+        degree: req.body.degree,
+        profilepicture: req.body.profilepicture
+    });
+    if(NewDoctor.save()){
+        console.log(NewDoctor);
+    }
+    res.send("Doctor details saved successfully");
+});
+
+app.get('/login_user', (req, res)=>{
+    const data = {
+        password:String(req.body.password),
+        email: String(req.body.email),
+    }
+    User.findOne({email:data.email}, (err, result)=>{
+        if(!err){
+            if(result != null){
+                if(result.password === data.password){
+                    res.send(true);
+                }
+                else{
+                    res.send(false);
+                }
+            }
+            else{
+                res.send("EXISTANCE_ERROR")
+            }
+        }
+        else{
+            res.send("INTERNAL_ERROR");
+        }
+    }); 
+});
+
+app.get('/login_doctor', (req, res)=>{
+    const data = {
+        password:String(req.body.password),
+        email: String(req.body.email),
+    }
+    Doctor.findOne({email:data.email}, (err, result)=>{
+        if(!err){
+            if(result != null){
+                if(result.password === data.password){
+                    res.send(true);
+                }
+                else{
+                    res.send(false);
+                }
+            }
+            else{
+                res.send("EXISTANCE_ERROR")
+            }
+        }
+        else{
+            res.send("INTERNAL_ERROR");
+        }
+    }); 
 });
 
 app.listen(port,()=>{
--- models/doctorModel.js
+++ models/doctorModel.js
@@ -1,16 +1,50 @@
 const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
-    name: String,
-    phone: String,
-    email: String,
-    birthday: Date,
-    gender: String,
-    bloodgroup: String,
-    experience: Number,
-    speciality: String,
-    dengree: String,
-    profilepicture: String
+    name: {
+        type:String,
+        required: true
+    },
+    phone: {
+        type:String,
+        required: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true
+    },
+    birthday: {
+        type:Date,
+        required: true
+    },
+    gender: {
+        type:String,
+        required: true
+    },
+    bloodgroup: {
+        type:String,
+        required: true
+    },
+    experience: {
+        type:String,
+        required: true
+    },
+    speciality: {
+        type:String,
+        required: true
+    },
+    degree: {
+        type:String,
+        required: true
+    },
+    profilepicture: {
+        type:String,
+        required: true
+    }
 });
 
 
